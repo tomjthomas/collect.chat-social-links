c1eb6a47183dfe0fd5a1a670b2f8d7bc40b862ea
tomjthomas <tomjthomas.1@gmail.com>
2020-04-21 03:41:36 +0530

    widget content gets generated from data

--- src/app.js
+++ src/app.js
@@ -1,17 +1,85 @@
-(function () {
-  const options = INSTALL_OPTIONS;
+import { values } from "./data.js";
+window.onload = function () {
+	const options = INSTALL_OPTIONS;
 
-  const widget = document.createElement("div");
-  widget.setAttribute("class", `widget ${options.location}`);
+	//temporary options till cloudflare works
+	let options = {
+		icon:
+			"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Ftransparent-round-icons%2F512%2Fchat.png&f=1&nofb=1",
+		location: "bottom-right",
+	};
 
-  const widgetButton = document.createElement("div");
-  widgetButton.style.backgroundImage = `url(${options.icon})`;
-  widgetButton.setAttribute("class", `widget-button ${options.location}`);
-  widgetButton.onclick = () => {
-    widget.style.display = widget.style.display === "none" ? "block" : "none";
-  };
+	let data = values();
 
-  const body = document.getElementsByTagName("body");
-  body[0].appendChild(widget);
-  body[0].appendChild(widgetButton);
-})();
+	//Widget Creation
+	const widget = document.createElement("div");
+	widget.setAttribute("class", `widget ${options.location}`);
+
+	//Fill the Widget
+
+	/* 
+	Structure of object ' '->variable names used
+	data={
+		'section':{
+			title:"",
+			content:{
+				'content':{
+					title:"",
+					content:"",
+					href:"",
+					icon:""
+				}
+			}
+		}
+	}
+	*/
+	for (let key of Object.keys(data)) {
+		let section = data[key];
+		let mainDiv = document.createElement("div");
+		if (key == "intro") {
+			mainDiv.innerHTML = `<h4 class="section-heading">${section["title"]}</h4>
+                                    <div class="section">
+                                        <div class="info">
+                                            <p class="in fo-heading">${section["content"]}</p>
+                                        </div>
+                                </div`;
+		} else {
+			mainDiv.innerHTML = `<h4 class="section-heading">${section["title"]}</h4>`;
+			let sectionDiv = document.createElement("div");
+			sectionDiv.setAttribute("class", "section");
+			Object.keys(section["content"]).forEach((key) => {
+				let content = section["content"][key];
+				console.log(content);
+				let a = document.createElement("a");
+				a.setAttribute("href", content["href"]);
+				a.innerHTML = `  <div class="info">
+                            <div class="info-icon-div">
+                                <span>${content["icon"]}</span>
+                            </div>
+                            <div class="info-text-div">
+                                <h5 class="info-heading">${content["title"]}</h5>
+                                <p class="info-text">
+                                ${content["content"]}
+                                </p>
+                            </div>
+                        </div>`;
+				sectionDiv.appendChild(a);
+			});
+			mainDiv.appendChild(sectionDiv);
+		}
+		widget.appendChild(mainDiv);
+	}
+
+	//Widget Button
+	const widgetButton = document.createElement("div");
+	widgetButton.style.backgroundImage = `url(${options.icon})`;
+	widgetButton.setAttribute("class", `widget-button ${options.location}`);
+	widgetButton.onclick = () => {
+		widget.style.display = widget.style.display === "none" ? "block" : "none";
+	};
+
+	//Add everything to body
+	const body = document.getElementsByTagName("body")[0];
+	body.appendChild(widget);
+	body.appendChild(widgetButton);
+};
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export function values() {
+	//TODO: code to add user values to the object
+	return {
+		intro: {
+			title: "INTRODUCTION",
+			content: "Welcome to our website!",
+		},
+		classic: {
+			title: "CLASSIC",
+			content: {
+				"e-mail": {
+					title: "E-Mail",
+					content: "tomjt99@gmail.com",
+					href: "mailto:tomjt99@gmail.com",
+					icon: '<i class="icons icon-envelope"></i>',
+				},
+				phone: {
+					title: "Phone Number",
+					content: "+918848111963",
+					href: "tel:918848111963",
+					icon: '<i class="icons icon-screen-smartphone"></i>',
+				},
+			},
+		},
+		social: {
+			title: "SOCIAL MEDIA",
+			content: {
+				facebook: {
+					title: "Facebook",
+					content: "facebook.com/tom.joseph.5496",
+					href: "https://www.facebook.com/tom.joseph.5496",
+					icon: '<i class="icons icon-social-facebook"></i>',
+				},
+				instagram: {
+					title: "Instagram",
+					content: "instagram.com/tom.j.thomas",
+					href: "https://www.instagram.com/tom.j.thomas/",
+					icon: '<i class="icons icon-social-instagram"></i>',
+				},
+			},
+		},
+	};
+}
